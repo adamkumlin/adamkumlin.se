074ab1f40acc6303abc43704bbd658c565796af3
adamkumlin <kumlinadam99@gmail.com>
2022-02-02 21:04:23 +0100

    2022-02-02

--- Adams_Webbplats/script.js
+++ Adams_Webbplats/script.js
@@ -6,7 +6,24 @@ toggleHamburgerMenu.addEventListener("click", () => {
 });
 
 const toggleDarkMode = document.getElementsByClassName("darkModeToggle")[0];
+const toggleLightMode = document.getElementsByClassName("lightModeToggle")[0];
 
 toggleDarkMode.addEventListener("click", () => {
+    toggleDarkMode.style.display ="none";
+    toggleLightMode.style.display ="inline";
     document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+
+    for (let i = 0; i < document.querySelectorAll(".navLinks li a").length; i++)
+        document.querySelectorAll(".navLinks li a")[i].style.color = "black";
+});
+
+toggleLightMode.addEventListener("click", () => {
+    toggleLightMode.style.display ="none";
+    toggleDarkMode.style.display ="inline";
+    document.body.style.backgroundColor = "";
+    document.body.style.color = "";
+
+    for (let i = 0; i < document.querySelectorAll(".navLinks li a").length; i++)
+        document.querySelectorAll(".navLinks li a")[i].style.color = "";
 });
