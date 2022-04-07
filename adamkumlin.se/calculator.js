753e6ebaa26663b50111c9e2f2166f279e36bf02
adamkumlin <kumlinadam99@gmail.com>
2022-04-07 16:55:50 +0200

    ew

---
+++ adamkumlin.se/calculator.js
@@ -0,0 +1,13 @@
+function calculate() {
+    let one = document.getElementById("number1").value;
+    let two = document.getElementById("number2").value;
+    let three = document.getElementById("number3").value;
+    let four = document.getElementById("number4").value;
+    let five = document.getElementById("number5").value;
+    let six = document.getElementById("number6").value;
+    let seven = document.getElementById("number7").value;
+    let eight = document.getElementById("number8").value;
+    let nine = document.getElementById("number9").value;
+
+    console.log(one + one);
+}
