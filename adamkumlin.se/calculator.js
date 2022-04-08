ebbe6077dabf277ac5f08b72c11fdeb794030db1
adamkumlin <kumlinadam99@gmail.com>
2022-04-09 01:05:06 +0200

    ew

--- adamkumlin.se/calculator.js
+++ adamkumlin.se/calculator.js
@@ -1,13 +1,81 @@
-function calculate() {
-    let one = document.getElementById("number1").value;
-    let two = document.getElementById("number2").value;
-    let three = document.getElementById("number3").value;
-    let four = document.getElementById("number4").value;
-    let five = document.getElementById("number5").value;
-    let six = document.getElementById("number6").value;
-    let seven = document.getElementById("number7").value;
-    let eight = document.getElementById("number8").value;
-    let nine = document.getElementById("number9").value;
+let result;
+let numberStorage = [];
 
-    console.log(one + one);
+let zero = document.getElementById("number0");
+let valueZero = 0;
+one.addEventListener("click", () => {
+    valueZero+= 0;
+    numberStorage.push(valueZero);
+})
+
+let one = document.getElementById("number1");
+let valueOne = 0;
+one.addEventListener("click", () => {
+    valueOne++;
+    numberStorage.push(valueOne);
+})
+
+let two = document.getElementById("number2");
+let valueTwo = 0;
+two.addEventListener("click", () => {
+    valueOne+= 2;
+    numberStorage.push(valueTwo);
+})
+
+let three = document.getElementById("number3");
+let valueThree = 0;
+three.addEventListener("click", () => {
+    valueThree+= 3;
+    numberStorage.push(valueThree);
+})
+
+let four = document.getElementById("number4");
+let valueFour = 0;
+four.addEventListener("click", () => {
+    valueFour+= 4;
+    numberStorage.push(valueFour);
+})
+
+let five = document.getElementById("number5");
+let valueFive = 0;
+five.addEventListener("click", () => {
+    valueFive+= 5;
+    numberStorage.push(valueFive);
+})
+
+let six = document.getElementById("number6");
+let valueSix = 0;
+six.addEventListener("click", () => {
+    valueSix+= 6;
+    numberStorage.push(valueSix);
+})
+
+let seven = document.getElementById("number7");
+let valueSeven = 0;
+seven.addEventListener("click", () => {
+    valueSeven+= 7;
+    numberStorage.push(valueSeven);
+})
+
+let eight = document.getElementById("number2");
+let valueEight = 0;
+eight.addEventListener("click", () => {
+    valueEight+= 8;
+    numberStorage.push(valueEight);
+})
+
+let nine = document.getElementById("number9");
+let valueNine = 0;
+nine.addEventListener("click", () => {
+    valueNine+= 9;
+    numberStorage.push(valueNine);
+})
+
+let add = document.getElementById("add");
+add.addEventListener("click", () => {
+    
+})
+
+function calculate() {
+
 }
