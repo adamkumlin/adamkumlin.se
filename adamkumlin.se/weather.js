186478493ed1ffd4a89eaa8fec3621920994f068
adamkumlin <kumlinadam99@gmail.com>
2022-04-11 00:25:56 +0200

    kkp

--- adamkumlin.se/weather.js
+++ adamkumlin.se/weather.js
@@ -6,23 +6,26 @@
     let kungsbackaButton = document.getElementById("kungsbacka");
     kungsbackaButton.addEventListener("click", () => {
         chosenCity = "Kungsbacka";
+        checkCity();
+        processWeatherData();
     });
 
     let deniaButton = document.getElementById("denia");
     deniaButton.addEventListener("click", () => {
         chosenCity = "Denia";
+        checkCity();
+        processWeatherData();
     })
 
-    if (chosenCity === "Kungsbacka") {
-        chosenLat = 57.4875;
-        chosenLon = 12.0762;
-
-        processWeatherData();
-    } else if (chosenCity === "Denia") {
-        chosenLat = 38.8388;
-        chosenLon = 0.1051;
-
-        processWeatherData();
+    function checkCity() {
+        if (chosenCity === "Kungsbacka") {
+            chosenLat = 57.4875;
+            chosenLon = 12.0762;
+    
+        } else if (chosenCity === "Denia") {
+            chosenLat = 38.8388;
+            chosenLon = 0.1051;
+        }
     }
 
     function processWeatherData() {
@@ -31,27 +34,22 @@
         .then(response => response.json())
         .then(data => {
     
-            console.log("data")
-    
             let city = document.getElementById("city");
             city.innerText = chosenCity;
     
             let weather = document.getElementById("weather");
-            weather.textContent = data.weather.main;
-    
-            let weatherDescription = document.getElementById("weatherDescription");
-            weatherDescription.textContent = data.weather.description;
-    
+            weather.textContent = data.weather[0].main + ", " + data.weather[0].description;
+
             let temp = document.getElementById("temp");
-            temp.textContent = data.main.temp;
+            temp.textContent = `Temperatur: ${parseInt(data.main.temp)} °C`;
     
             let tempMax = document.getElementById("tempMax");
-            tempMax.textContent = data.main.temp_max;
+            tempMax.textContent = `Högsta temperatur: ${parseInt(data.main.temp_max)} °C`;
     
             let tempMin = document.getElementById("tempMin");
-            tempMin.textContent = data.main.temp_min;
+            tempMin.textContent = `Lägsta temperatur: ${parseInt(data.main.temp_min)} °C`;
     
             let tempFeelsLike = document.getElementById("tempFeelsLike");
-            tempFeelsLike.textContent = data.main.feels_like;
+            tempFeelsLike.textContent = `Känns som: ${parseInt(data.main.feels_like)} °C`;
         });
     }
