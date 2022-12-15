e61603ccb50fa77ebc2acc5f477ffaf7d2af21a2
adamkumlin <kumlinadam99@gmail.com>
2022-12-15 20:20:52 +0100

    update nav

--- adamkumlin.se/src/components/NavBar.js
+++ adamkumlin.se/src/components/NavBar.js
@@ -1,20 +1,15 @@
 import { Link } from "react-router-dom";
-import { useRef } from "react";
 
 function NavBar() {
 
-    const activeLink = useRef();
-
-    function handleActiveLink() {
-        console.log(activeLink.current);
-        
-    }
   return (
     <div className="NavBar">
-        <Link ref={activeLink} onMouseEnter={handleActiveLink} to="/contact">KONTAKTA MIG</Link>
-        <Link ref={activeLink} onMouseEnter={handleActiveLink} to="/about">OM MIG</Link>
-        <Link ref={activeLink} onMouseEnter={handleActiveLink} to="/projects">TIDIGARE PROJEKT</Link>
-        <Link ref={activeLink} onMouseEnter={handleActiveLink} to="/cv">CV</Link>
+      <div className="navLinkContainer">
+        <Link className="navLink" to="/contact">KONTAKTA MIG</Link>
+        <Link className="navLink" to="/about">OM MIG</Link>
+        <Link className="navLink" to="/projects">TIDIGARE PROJEKT</Link>
+        <Link className="navLink" to="/cv">CV</Link>
+      </div>
     </div>
   );
 }
--- adamkumlin.se/src/components/pages/Home.js
+++ adamkumlin.se/src/components/pages/Home.js
@@ -1,8 +1,12 @@
 import NavBar from "../NavBar";
+import backgroundVideo from "../../images/background-video.mp4";
 
 function Home() {
     return (
       <div className="Home">
+        <video className="backgroundVideo" autoplay muted>
+          <source src={backgroundVideo} type="video/mp4"/>
+        </video>
         <NavBar/>
       </div>
     );
