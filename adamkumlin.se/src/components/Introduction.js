f9ce34e33ccc3aeaa20247c0a049701e46b9f4bb
adamkumlin <kumlinadam99@gmail.com>
2022-12-21 20:02:47 +0100

    finish animation for nav button and intro

--- adamkumlin.se/src/components/Introduction.js
+++ adamkumlin.se/src/components/Introduction.js
@@ -3,7 +3,7 @@ function Introduction() {
     return (
       <div className="Introduction">
           <h1>Adam Kumlin</h1>
-          <h2>Programmerare och utvecklare</h2>
+          <h2></h2>
       </div>
     );
   }
