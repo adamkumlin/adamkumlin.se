1319bfcb35ae86e1bb399f96d910df79012e3798
adamkumlin <kumlinadam99@gmail.com>
2023-01-03 00:25:37 +0100

    work on contact options

---
+++ src/components/ContactOptions.js
@@ -0,0 +1,13 @@
+function ContactOptions() {
+
+    return (
+      <div className="ContactOptions">
+        <div className="email">
+            <div className="envelopeTop"></div>
+            <div className="envelopeBottom"></div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default ContactOptions;
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,20 +1,23 @@
 import backgroundVideo from "../../images/background-video.mp4";
 import BackButton from "../BackButton";
+import ContactOptions from "../ContactOptions";
 
 function Contact() {
     return (
       <div className="Contact">
 
         <div className="background">
-          <div lassName="backgroundOverlay"></div>
+          <div className="backgroundOverlay"></div>
           <video className="backgroundVideo" autoPlay="autoPlay" muted="muted" loop="loop">
               <source src={backgroundVideo} type="video/mp4"/>
           </video>
         </div>
 
-        <h2>Kontakta mig</h2>
+        <h2 className="heading">Kontakta mig</h2>
 
         <BackButton/>
+
+        <ContactOptions/>
       </div>
     );
   }
