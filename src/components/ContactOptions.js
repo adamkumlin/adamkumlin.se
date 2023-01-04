ad3fa49f19a1c329f410eebf0d408e8109f42a60
adamkumlin <kumlinadam99@gmail.com>
2023-01-04 17:53:28 +0100

    work on contact page

--- src/components/ContactOptions.js
+++ src/components/ContactOptions.js
@@ -1,13 +1,26 @@
-function ContactOptions() {
+import { forwardRef } from "react";
+import { Link } from "react-router-dom";
+import githubIcon from "../images/github-icon.svg";
 
-    return (
-      <div className="ContactOptions">
-        <div className="email">
-            <div className="envelopeTop"></div>
-            <div className="envelopeBottom"></div>
-        </div>
-      </div>
-    );
-  }
+const ContactOptions = forwardRef((props, ref) => {
+
+  const {ref0, ref1, ref2} = ref;
+
+  return (
+
+    <div className="ContactOptions">
+      <Link target="_blank" onClick={() => {window.location.href = "mailto:kumlinadam99@gmail.com";}} className="email" onMouseEnter={props.playEmailAnimation}>
+          <div ref={ref0} className="envelopeTop"></div>
+          <div ref={ref1} className="envelopeMiddle"></div>
+          <div className="envelopeBottom"></div>
+          <span ref={ref2} className="emailTooltip">E-post</span>
+      </Link>
+
+      <Link target="_blank" onClick={() => {window.location.href = "https://github.com/adamkumlin"}} className="github">
+        <img src={githubIcon} alt="GitHub icon"/>
+      </Link>
+    </div>
+  );
   
+});
   export default ContactOptions;
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,8 +1,31 @@
 import backgroundVideo from "../../images/background-video.mp4";
 import BackButton from "../BackButton";
 import ContactOptions from "../ContactOptions";
+import { useRef } from "react";
 
 function Contact() {
+
+  const emailTopRef = useRef(null);
+  const emailMiddleRef = useRef(null);
+  const emailTooltipRef = useRef(null);
+
+  function playEmailAnimation() {
+    emailTopRef.current.style.animation = "openEnvelopeTop 0.5s forwards linear";
+    emailMiddleRef.current.style.animation = "openEnvelopeMiddle 1s forwards";
+    emailMiddleRef.current.style.animationDelay = "0.6s";
+
+    window.onmousemove = function(e) {
+      let x = (e.clientX + 20) + "px";
+      let y = (e.clientY + 20) + "px";
+
+      for (let i = 0; i < emailTooltipRef.current.textContent.length; i++) {
+        emailTooltipRef.current.style.top = y;
+        emailTooltipRef.current.style.left = x;
+
+    }
+  }
+}
+
     return (
       <div className="Contact">
 
@@ -17,7 +40,7 @@ function Contact() {
 
         <BackButton/>
 
-        <ContactOptions/>
+        <ContactOptions playEmailAnimation={playEmailAnimation} ref={{ref0: emailTopRef, ref1: emailMiddleRef, ref2: emailTooltipRef}}/>
       </div>
     );
   }
