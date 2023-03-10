4aac074f3819a1f343c44877a4c6aa78fb5c0b0e
Adam Kumlin <kumlinadam99@gmail.com>
2023-03-11 00:57:56 +0100

    style cv and about pages

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -11,12 +11,15 @@ function About() {
           
           <h2 className="heading">Hej!</h2>
 
-          <p>
-            Välkommen till min webbplats! Jag heter Adam och är mycket intresserad av webbutveckling och övrig programmering. Jag går just nu sista året på Webbredaktörsprogrammet på
-            Högskolan i Borås. Genom min utbildning har jag lärt mig programmering med JavaScript samt kodning med HTML och CSS. Jag är också kunnig inom flera frontend-frameworks som React.
+          <p> Välkommen till min webbplats!</p>
+          
+          <p>Jag heter Adam och är mycket intresserad av webbutveckling och övrig programmering.</p>
+          
+          <p>Jag går just nu sista året på Webbredaktörsprogrammet på Högskolan i Borås. Genom min utbildning har jag lärt mig programmering med JavaScript samt kodning med HTML och CSS. Jag är också kunnig inom flera frontend-frameworks som React.
             Utbildningen har också givit mig kunskap inom redaktionellt arbete som skrivande och fotoredigering. Slutligen har användbarhet, att en produkt ska kunna vara tillgänglig och enkel att använda,
-            varit centralt under hela utbildningen. Jag är också relativt kunnig gällande backend-utveckling med PHP, Python och Node.js, samt med databasspråket SQL.
-          </p>
+            varit centralt under hela utbildningen. </p>
+            
+            <p>Jag är också relativt kunnig gällande backend-utveckling med PHP, Python och Node.js, samt med databasspråket SQL.</p>
         <Footer/>
       </div>
     );
