c07f671cde77212cc3ca34f31e7e60678095a966
Adam Kumlin <kumlinadam99@gmail.com>
2023-03-13 17:26:01 +0100

    add more buttons to calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,9 +7,9 @@ function Calculator() {
     const [previousNumber, setPreviousNumber] = useState(0);
     const [operator, setOperator] = useState(0);
 
-    function handleChange(e) {
+    function handleInput(e) {
 
-        if (number == 0) {
+        if (number === 0) {
             setNumber(e.target.value);
         } else {
             setNumber(number + e.target.value);
@@ -54,31 +54,45 @@ function Calculator() {
         <div>{eventList}</div>
 
         <div className="row1">
-            <button value={9} onClick={handleChange}>9</button>
-            <button value={8} onClick={handleChange}>8</button>
-            <button value={7} onClick={handleChange}>7</button>
-        </div>
-        <div className="row2">
-            <button value={6} onClick={handleChange}>6</button>
-            <button value={5} onClick={handleChange}>5</button>
-            <button value={4} onClick={handleChange}>4</button>
+            <button value={"pi"} onClick={handleInput}>&#960;</button>
+            <button value={"log"} onClick={handleInput}>log</button>
+            <button value={"ln"} onClick={handleInput}>ln</button>
+            <button value={"%"} onClick={handleInput}>%</button>
+            <button value={"c"} onClick={handleInput}>C</button>
+            <button onClick={reset}>CE</button>
         </div>
 
-        <div className="row3">
-            <button value={3} onClick={handleChange}>3</button>
-            <button value={2} onClick={handleChange}>2</button>
-            <button value={1} onClick={handleChange}>1</button>
+        <div className="row2">
+            <button value={"e"} onClick={handleInput}>e</button>
+            <button value={"cos"} onClick={handleInput}>cos</button>
+            <button value={9} onClick={handleInput}>9</button>
+            <button value={8} onClick={handleInput}>8</button>
+            <button value={7} onClick={handleInput}>7</button>
+            <button value={"+"}onClick={changeOperation}>+</button>
         </div>
+        <div className="row3">
+            <button value={"x!"} onClick={handleInput}>x!</button>
+            <button value={"tan"} onClick={handleInput}>tan</button>
+            <button value={6} onClick={handleInput}>6</button>
+            <button value={5} onClick={handleInput}>5</button>
+            <button value={4} onClick={handleInput}>4</button>
+            <button value={"-"}onClick={changeOperation}>-</button>
+        </div>
+
         <div className="row4">
-            <button value={0} onClick={handleChange}>0</button>
+            <button value={"root"} onClick={handleInput}>&#8730;</button>
+            <button value={"sin"} onClick={handleInput}>sin</button>
+            <button value={3} onClick={handleInput}>3</button>
+            <button value={2} onClick={handleInput}>2</button>
+            <button value={1} onClick={handleInput}>1</button>
+            <button value={"*"}onClick={changeOperation}>*</button>
+        </div>
+        <div className="row5">
+            <button value={"ans"} onClick={handleInput}>ANS</button>
+            <button value={"/"}onClick={changeOperation}>x<sup>y</sup></button>
+            <button value={0} onClick={handleInput}>0</button>
             <button value={"."}onClick={changeOperation}>.</button>
             <button value={"="}onClick={calculate}>=</button>
-        </div>
-        <div className="aside1">
-            <button onClick={reset}>CE</button>
-            <button value={"+"}onClick={changeOperation}>+</button>
-            <button value={"-"}onClick={changeOperation}>-</button>
-            <button value={"*"}onClick={changeOperation}>*</button>
             <button value={"/"}onClick={changeOperation}>/</button>
         </div>
     </div>
