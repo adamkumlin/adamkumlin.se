b50e017796ca42124aee28986e749fc78f4d51a2
Adam Kumlin <kumlinadam99@gmail.com>
2023-03-15 01:14:35 +0100

    work on more buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -44,6 +44,24 @@ function Calculator() {
 
             case "/": setNumber(parseFloat(previousNumber) / parseFloat(number));
                 break;
+
+            case "^": setNumber(Math.pow(parseFloat(previousNumber), parseFloat(number)));
+                break;
+
+            case "sin": setNumber(Math.sin(parseFloat(number)));
+                break;
+
+            case "cos": setNumber(Math.cos(parseFloat(previousNumber)));
+                break;
+
+            case "tan": setNumber(Math.tan(parseFloat(previousNumber)));
+                break;
+
+            case "ln": setNumber(Math.log(parseFloat(number)));
+                break;
+
+            case "log": setNumber(Math.log10(parseFloat(number)));
+                break;
         }
     }
 
@@ -54,17 +72,17 @@ function Calculator() {
         <div>{eventList}</div>
 
         <div className="row1">
-            <button value={"pi"} onClick={handleInput}>&#960;</button>
-            <button value={"log"} onClick={handleInput}>log</button>
-            <button value={"ln"} onClick={handleInput}>ln</button>
+            <button value={Math.PI} onClick={handleInput}>&#960;</button>
+            <button value={"log"} onClick={changeOperation}>log</button>
+            <button value={"ln"} onClick={changeOperation}>ln</button>
             <button value={"%"} onClick={handleInput}>%</button>
-            <button value={"c"} onClick={handleInput}>C</button>
+            <button value={"exp"} onClick={handleInput}>EXP</button>
             <button onClick={reset}>CE</button>
         </div>
 
         <div className="row2">
             <button value={"e"} onClick={handleInput}>e</button>
-            <button value={"cos"} onClick={handleInput}>cos</button>
+            <button value={"cos"} onClick={changeOperation}>cos</button>
             <button value={9} onClick={handleInput}>9</button>
             <button value={8} onClick={handleInput}>8</button>
             <button value={7} onClick={handleInput}>7</button>
@@ -72,7 +90,7 @@ function Calculator() {
         </div>
         <div className="row3">
             <button value={"x!"} onClick={handleInput}>x!</button>
-            <button value={"tan"} onClick={handleInput}>tan</button>
+            <button value={"tan"} onClick={changeOperation}>tan</button>
             <button value={6} onClick={handleInput}>6</button>
             <button value={5} onClick={handleInput}>5</button>
             <button value={4} onClick={handleInput}>4</button>
@@ -81,7 +99,7 @@ function Calculator() {
 
         <div className="row4">
             <button value={"root"} onClick={handleInput}>&#8730;</button>
-            <button value={"sin"} onClick={handleInput}>sin</button>
+            <button value={"sin"} onClick={changeOperation}>sin</button>
             <button value={3} onClick={handleInput}>3</button>
             <button value={2} onClick={handleInput}>2</button>
             <button value={1} onClick={handleInput}>1</button>
@@ -89,7 +107,7 @@ function Calculator() {
         </div>
         <div className="row5">
             <button value={"ans"} onClick={handleInput}>ANS</button>
-            <button value={"/"}onClick={changeOperation}>x<sup>y</sup></button>
+            <button value={"^"}onClick={changeOperation}>x<sup>y</sup></button>
             <button value={0} onClick={handleInput}>0</button>
             <button value={"."}onClick={changeOperation}>.</button>
             <button value={"="}onClick={calculate}>=</button>
