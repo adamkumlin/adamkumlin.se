1c67286db2c395d6dc14ad8bdd6773976a20330c
Adam Kumlin <kumlinadam99@gmail.com>
2023-05-30 19:53:15 +0200

    delete .php files

--- database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  $servername = "localhost";
-  $username = "uqafhyuz";
-  $password = "62aAEy7)]eh0WJ";
-  $database = "uqafhyuz_database";
-
-  $conn = mysqli_connect($servername, $username, $password, $database);
-?> 
